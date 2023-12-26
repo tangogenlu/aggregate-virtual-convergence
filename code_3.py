a88edae1e26977c39e022b4af8c94125be968d93
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2023-12-26 14:26:22 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,70 @@
-    return data
-    data = generate_random_data()
 
+    for item in data:
+        print(f"Random Number: {item}")
 def main():
-    data = [random.randint(1, 100) for _ in range(10)]
     main()
 
-        print(f"Random Number: {item}")
-    for item in data:
-def generate_random_data():
 
-if __name__ == "__main__":
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
 import random
+    return data
+if __name__ == "__main__":
+    data = generate_random_data()        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        }
+    }
+        for (int item : data) {
+}
+        }
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+}
+        for (int item : data) {
+    }
+
+        }
+
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {    });
+
+    data.forEach(item => {
+
+}
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+function main() {
+}
+
+    const data = [];
+        console.log("Random Number: " + item);
+main();
+    }
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;main();
+function main() {
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+    return data;
+
+    data.forEach(item => {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+    }
+
+function generateRandomData() {
+        console.log("Random Number: " + item);
+}
+}
+    const data = [];
